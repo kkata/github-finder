d58eae75b8d7e8e64123c076d44551d90e3fb16f
kkata <14849535+kkata@users.noreply.github.com>
2022-04-18 19:28:35 +0900

    userlist component

---
+++ src/components/users/UserResults.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import { Endpoints } from "@octokit/types";
+import { Spinner } from "../layout/Spinner";
+
+type listUsers = Endpoints["GET /users"]["response"]["data"];
+
+export const UserResults = () => {
+  const [users, setUsers] = useState<listUsers>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    const res = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
+      headers: {
+        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+      },
+    });
+    const data = await res.json();
+    setUsers(data);
+    setLoading(false);
+  };
+
+  if (!loading) {
+    return (
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+        {users.map((user, index) => (
+          <h3 key={index}>{user.login}</h3>
+        ))}
+      </div>
+    );
+  } else {
+    return <Spinner />;
+  }
+};
