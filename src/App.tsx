1c37185ff9ed161a5944095e767880345e7a6f36
kkata <14849535+kkata@users.noreply.github.com>
2022-04-18 11:49:46 +0900

    setup tailwind and daisy ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-purple-500">
+      <h1 className="text-xl">Hello world</h1>
     </div>
   );
-}
-
-export default App;
+};
