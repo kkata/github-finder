8d77b5c717c038a678e0c5830ff72fa0d7d7a30e
kkata <14849535+kkata@users.noreply.github.com>
2022-04-20 09:59:24 +0900

    display users

--- src/components/users/UserResults.tsx
+++ src/components/users/UserResults.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Endpoints } from "@octokit/types";
 import { Spinner } from "../layout/Spinner";
+import { UserItem } from "./UserItem";
 
 type listUsers = Endpoints["GET /users"]["response"]["data"];
 
@@ -26,8 +27,8 @@ export const UserResults = () => {
   if (!loading) {
     return (
       <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
-        {users.map((user, index) => (
-          <h3 key={index}>{user.login}</h3>
+        {users.map((user) => (
+          <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
